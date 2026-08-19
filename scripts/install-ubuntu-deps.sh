fc58f1848ff3505f147c2ed3fd4ec5e3f3fdca7c
Nazar Yestayev <legion2440@mail.ru>
2026-08-19 11:47:54 +0500

    complete Ubuntu Qt runtime dependencies

--- scripts/install-ubuntu-deps.sh
+++ scripts/install-ubuntu-deps.sh
@@ -6,6 +6,7 @@ sudo apt install -y \
   build-essential \
   cmake \
   ninja-build \
+  libxkbcommon-dev \
   qt6-base-dev \
   qt6-declarative-dev \
   qt6-tools-dev \
@@ -14,4 +15,6 @@ sudo apt install -y \
   qml6-module-qtqml-workerscript \
   qml6-module-qtquick \
   qml6-module-qtquick-controls \
-  qml6-module-qtquick-layouts
+  qml6-module-qtquick-layouts \
+  qml6-module-qtquick-templates \
+  qml6-module-qtquick-window
