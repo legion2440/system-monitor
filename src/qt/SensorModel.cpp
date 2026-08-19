27d312e514b4f3f73efa36c35687e425b631b285
Nazar Yestayev <legion2440@mail.ru>
2026-08-19 16:43:34 +0500

    expose sensor chip metadata

--- src/qt/SensorModel.cpp
+++ src/qt/SensorModel.cpp
@@ -5,8 +5,8 @@ int SensorModel::rowCount(const QModelIndex& parent) const{return parent.isValid
 QVariant SensorModel::data(const QModelIndex& index,int role) const{
     if(!index.isValid()||index.row()<0||index.row()>=static_cast<int>(sensors_.size()))return{};
     const auto& s=sensors_[static_cast<std::size_t>(index.row())];
-    switch(role){case NameRole:return QString::fromStdString(s.name);case SourceRole:return QString::fromStdString(s.source);case UnitRole:return QString::fromStdString(s.unit);case ValueRole:return s.value;default:return{};}
+    switch(role){case NameRole:return QString::fromStdString(s.name);case ChipRole:return QString::fromStdString(s.chip);case SourceRole:return QString::fromStdString(s.source);case UnitRole:return QString::fromStdString(s.unit);case ValueRole:return s.value;default:return{};}
 }
-QHash<int,QByteArray> SensorModel::roleNames() const{return{{NameRole,"name"},{SourceRole,"source"},{UnitRole,"unit"},{ValueRole,"value"}};}
+QHash<int,QByteArray> SensorModel::roleNames() const{return{{NameRole,"name"},{ChipRole,"chip"},{SourceRole,"source"},{UnitRole,"unit"},{ValueRole,"value"}};}
 void SensorModel::updateSensors(std::vector<SensorInfo> sensors){beginResetModel();sensors_=std::move(sensors);endResetModel();}
 }
